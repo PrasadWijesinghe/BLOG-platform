ff80f25e73c71c3fd8992fd1fd9bc676e9356ae7
PrasadWijesinghe <wmprasadwijesinghe@gmail.com>
2026-02-07 23:53:19 +0530

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-import express from "express";
 import dotenv from "dotenv";
-const app = express();
 dotenv.config();
 
-import userRoutes from "./src/routes/userroutes.js";
+import express from "express";
 
+import userRoutes from "./src/routes/userroutes.js";
 import authRoutes from "./src/routes/authroutes.js";
 import blogRoutes from "./src/routes/blogroutes.js";
 
+const app = express();
 const PORT = process.env.PORT || 5000;
-
 
 app.use(express.json());
 
@@ -17,11 +16,6 @@ app.use("/auth", authRoutes);
 app.use("/blogs", blogRoutes);
 app.use("/users", userRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Backend is running");
-});
+app.get("/", (req, res) => res.send("Backend is running"));
 
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
